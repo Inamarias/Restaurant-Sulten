e84770e6d0564f7892001b6bc5701df7e90e6a16
camona <103410211+camona@users.noreply.github.com>
2022-11-12 15:47:24 +0100

    litt meir fiksings

--- javascript/myFile.js
+++ javascript/myFile.js
@@ -1,21 +1,49 @@
 // Hentet fra: https://www.w3schools.com/howto/howto_js_slideshow.asp
 
-let slideIndex = 0;
+var slideIndex = 0;
 showSlides();
+var slides, dots;
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("bilde-slideshow");
-  let dots = document.getElementsByClassName("dot");
+    var i;
+    slides = document.getElementsByClassName("bilde-slideshow");
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 8000); // Change image every 8 seconds
+}
+
+function plusSlides(position) {
+  slideIndex +=position;
+  if (slideIndex> slides.length) {slideIndex = 1}
+  else if(slideIndex<1){slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+     slides[i].style.display = "none";  
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Forandrer bilde hvert 5. sekund
+}
+
+function currentSlide(index) {
+  if (index> slides.length) {index = 1}
+  else if(index<1){index = slides.length}
+  for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[index-1].style.display = "block";  
+  dots[index-1].className += " active";
 }
